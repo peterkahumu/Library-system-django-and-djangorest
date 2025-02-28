51f4d09c4a29f969baace6d8597bfe31b087ce06
Peter Muhumuki <muhumukip@gmail.com>
2025-02-28 14:58:54 +0300

    Implement Book model and views; update URL routing and templates for book listing

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'isbn']
+    search_fields = ['id', 'isbn', 'isbn', 'author']
+    list_filter = ['author', ]
