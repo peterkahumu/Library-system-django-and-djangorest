590dc7b359917d45fad0a781c19ceedf8a4ecf98
Peter Muhumuki <muhumukip@gmail.com>
2025-02-28 15:13:40 +0300

    Add 'api' app with initial configuration, URL routing, and basic structure

--- Library/settings.py
+++ Library/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+    'rest_framework',
+
+    # installed apps locally.
     'books',
+    'api',
 ]
 
 MIDDLEWARE = [
